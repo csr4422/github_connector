77b9d88b56eb56ac2635beb2f0bcf6fc6a728778
csr4422 <csr935640@gmail.com>
2026-04-02 15:13:51 +0530

    feat: add get commits endpoint

--- app/routes/repos.py
+++ app/routes/repos.py
@@ -2,8 +2,12 @@ from fastapi import APIRouter
 from app.services import github_client
 
 
-router =APIROUTER()
+router =APIRouter()
 
 @router.get("/repos")
 def get_repo():
     return github_client.get("user/repos")
+
+@router.get("/repos/{owner}/{repo}/commits")
+def get_commits(owner:str,repo:str):
+    return github_client.get(f"/repos/{owner}/{repo}/commits")
