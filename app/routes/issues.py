6b6f76e85b3730d2be40a8c5548dce6c55174b75
csr4422 <csr935640@gmail.com>
2026-04-02 16:07:15 +0530

    feat :add get issues endpoint

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -3,3 +3,7 @@ from app.services import github_client
 from app.schemas.issues import CreateIssueRequest, CreateIssueResponse
 
 router= APIRouter()
+
+@router.get("repos/{owner}/{repo}/issues")
+def get_issues(owner:str,repo:str):
+    return github_client.get(f"repos/{owner}/{repo}/issues")
