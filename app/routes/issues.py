1d586f637698a4eb47fb602e9076f2ff8c020a3e
csr4422 <csr935640@gmail.com>
2026-04-02 17:29:22 +0530

    feat: add create issue endpoint

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -7,3 +7,10 @@ router= APIRouter()
 @router.get("repos/{owner}/{repo}/issues")
 def get_issues(owner:str,repo:str):
     return github_client.get(f"repos/{owner}/{repo}/issues")
+
+@router.post("/repo/{owner}/{repo}/issues",response_model=CreateIssueResponse)
+def ceate_issue(owner:str,repo:str,issue:CreateIssueRequest):
+    return github_client.post(
+        f"repos/{owner}/{repo}/issues",
+        body=issue.model_dump(exclude_none=True),
+    )
