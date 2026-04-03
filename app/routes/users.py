30a2db18d5fc0cd152ea1245c67c3a0a30bb1d6a
csr4422 <csr935640@gmail.com>
2026-04-03 15:08:03 +0530

    feat: add get authenticated user endpoint

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,17 @@
-from fastAPI import APIRouter
+from fastapi import APIRouter
 from app.services import github_client
 
 router = APIRouter()
+
+@router.get("/user")
+def get_user():
+    user = github_client.get("/user")
+    return {
+        "login": user["login"],
+        "name": user["name"],
+        "avatar_url": user["avatar_url"],
+        "public_repos": user["public_repos"],
+        "followers": user["followers"],
+        "following": user["following"],
+        "url": user["html_url"],
+    }
