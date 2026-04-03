342113e578c27fd261df8382adb7997645bf200c
csr4422 <csr935640@gmail.com>
2026-04-03 15:13:52 +0530

    feat: register users router in main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import repos, issues
+from app.routes import repos, issues, users
 
 app = FastAPI(
     title="Github Connector",
@@ -9,7 +9,7 @@ app = FastAPI(
 
 app.include_router(repos.router)
 app.include_router(issues.router)
-
+app.include_router(users.router)
 @app.get("/health")
 def health_check():
     return{"status":"ok", "message":"Github Connector is running"}
